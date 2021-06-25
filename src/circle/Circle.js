2adeb37d35c8d6f2686ef182961fc4ead765462a
FountainShaw <zhulin_525@126.com>
2021-06-25 17:44:38 +0800

    1 file modified

--- src/circle/Circle.js
+++ src/circle/Circle.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Slider } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Input, Slider, Button } from 'antd';
 import sha256 from 'crypto-js/sha256';
 
 // 最外层圆的半径
@@ -59,23 +59,53 @@ function drawBlock(block) {
   ].join(' ');
 }
 
-// 缓存各层圆弧的角度数据
-let arcCache;
+// 获取各个小块的hsl值
+function getHsl(hash) {
+  // 1.分割hash，获取各层和各模块的值
+  const hashSplice = len => {
+    let temp = hash;
+    const spliceLength = temp.length / len;
+    return [...Array(len).keys()].map(item =>
+      temp.slice(item * spliceLength, (item + 1) * spliceLength)
+    );
+  };
+  const rings = hashSplice(4);
+  const blocks = hashSplice(32);
+
+  // 2.计算hash、ring、block的特征
+  const blocksNum = blocks.map(item => parseInt(item, 16));
+  const oxReducer = (acc, cur) => cur ^ acc;
+
+  const hashFeature = (blocksNum.reduce(oxReducer, 0) * 100) / 255;
+  const ringsFeature = rings.map((_, i) => {
+    const temp = blocksNum.slice(i * 8, (i + 1) * 8);
+    return (temp.reduce(oxReducer, 0) * 250) / 255;
+  });
+  const blocksFeature = blocksNum.map(item => (item * 10) / 255);
+
+  // 3.通过特征数据计算出各个块hsl的值
+  return blocksFeature.map((item, index) => {
+    const ringIndex = Math.floor(index / 8);
+    const hue = Math.floor(hashFeature + ringsFeature[ringIndex] + item);
+    const saturation = (blocksNum[index] * 60) / 255 + 40;
+    const lightness = (blocksNum[index] * 70) / 255 + 30;
+
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  });
+}
 
 // 封装拆分的各个圆弧的相关数据
-function wrapArcData(radius, hash) {
-  console.log(hash);
+function wrapArcData(radius, hash, disorder = 0) {
+  const hsl = getHsl(hash);
   const range = [...Array(8).keys()];
-
-  if (!arcCache) {
-    arcCache = range.map(item => ({
-      x: Math.cos((Math.PI * item) / 4),
-      y: Math.sin((Math.PI * item) / 4)
-    }));
-  }
 
   const blockInfo = radius.map((r, i) => {
     const innerRadius = i + 1 >= radius.length ? 0 : radius[i + 1];
+    const arcCache = range.map(j => ({
+      x: Math.cos((Math.PI * (j + i * disorder)) / 4),
+      y: Math.sin((Math.PI * (j + i * disorder)) / 4)
+    }));
+
     return arcCache.map((item, index) => {
       const nextIndex = index + 1 >= arcCache.length ? 0 : index + 1;
       const outterNext = arcCache[nextIndex];
@@ -93,10 +123,7 @@ function wrapArcData(radius, hash) {
       };
       return {
         path: drawBlock(block),
-        fill: `hsl(${Math.floor(
-          (((block.outterRIngStartY + block.outterRingEndY) / 2 + 100) * 360) /
-            200
-        )}, ${60}%, ${60}%)`
+        fill: hsl[i * 8 + index]
       };
     });
   });
@@ -107,17 +134,48 @@ function wrapArcData(radius, hash) {
 export default function Circle() {
   // 协同使用等面积与等半径情况下的半径的混合因子
   const [mix, setMix] = useState(0.42);
+  const [disorder, setDisorder] = useState(0);
+  const [user, setUser] = useState('Fountain Shaw');
+  const [blockInfo, setBlockInfo] = useState([]);
+
   const radius = areaArr.map((_, index) => getRadius(index, mix));
-  const hash = sha256('Message').toString();
-  const blockInfo = wrapArcData(radius, hash);
+  const generatArcData = () => {
+    const hash = sha256(user).toString();
+    setBlockInfo(wrapArcData(radius, hash, disorder));
+  };
+
+  useEffect(generatArcData, [disorder, mix]);
 
   return (
-    <div style={{ padding: '30px' }}>
+    <div style={{ padding: '50px' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>用户名：</span>
+        <Input
+          style={{ flex: 4 }}
+          value={user}
+          onChange={e => setUser(e.target.value)}
+          onPressEnter={generatArcData}
+        />
+        <Button
+          style={{ marginLeft: '10px' }}
+          type="primary"
+          size={'small'}
+          onClick={generatArcData}
+        >
+          确认
+        </Button>
+      </div>
       <Slider
         value={mix * 100}
         tooltipVisible={false}
-        marks={{ [mix * 100]: `混合因子：${Math.floor(mix * 100)}%` }}
+        marks={{ [mix * 100]: `尺寸因子：${Math.floor(mix * 100)}%` }}
         onChange={val => setMix(val / 100)}
+      />
+      <Slider
+        value={disorder * 100}
+        tooltipVisible={false}
+        marks={{ [disorder * 100]: `旋转因子：${Math.floor(disorder * 100)}%` }}
+        onChange={val => setDisorder(val / 100)}
       />
       <svg viewBox={`${-R} ${-R} ${2 * R} ${2 * R}`}>
         {blockInfo.map(item => (
@@ -126,7 +184,7 @@ export default function Circle() {
             d={item.path}
             fill={item.fill}
             stroke={'white'}
-            strokeWidth={'1'}
+            strokeWidth={'0.5'}
           />
         ))}
       </svg>
